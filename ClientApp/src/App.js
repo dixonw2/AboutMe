359fda749559320b1150b6bd646cdf610a632754
dixonw2 <mrbacon2015@gmail.com>
2021-10-02 19:14:20 -0400

    Initial commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+import { NotFoundPage } from './components/NotFoundPage';
+
+import './custom.css'
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/counter' component={Counter} />
+          <Route path='/fetch-data' component={FetchData} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Layout>
+    );
+  }
+}
