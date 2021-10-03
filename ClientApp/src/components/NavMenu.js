58abed23f0bbc5a7db8a08c95197b78986dd6d7a
dixonw2 <mrbacon2015@gmail.com>
2021-10-03 14:30:39 -0400

    Change bootstrap components from reactstrap to React Bootstrap

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -24,23 +23,16 @@ export class NavMenu extends Component {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+        <Navbar bg='dark' variant='dark'>
           <Container>
-            <NavbarBrand tag={Link} to="/">AboutMe</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
+          <Navbar.Brand href='/'>Wyatt Dixon</Navbar.Brand>
+            <Nav>
+              <Nav.Link href='/counter'>Counter</Nav.Link>
+              <Nav.Link href='/fetch-data'>Weather</Nav.Link>
+              <NavDropdown title='Music'>
+                <NavDropdown.Item href='/music/favoritesongs'>Favorite Songs of the Year</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
           </Container>
         </Navbar>
       </header>
