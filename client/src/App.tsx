938e53da2a7088b9cc4224761f3a92a29a40b22d
dixonw2 <mrbacon2015@gmail.com>
2025-09-08 14:48:27 -0400

    Recreate client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,6 +29,5 @@ export default function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
-
